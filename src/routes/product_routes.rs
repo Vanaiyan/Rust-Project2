d2f2496734e7acafb8387bff52f629d8a9ba74d1
Vanaiyan <kvanaiyan2000@gmail.com>
2024-12-16 17:41:18 +0530

    testing automated deployment

--- src/routes/product_routes.rs
+++ src/routes/product_routes.rs
@@ -5,12 +5,12 @@ use crate::handlers::product_hanlers::{
     get_product_by_uuid, 
     delete_product, 
     update_product};
-    
+
 use axum::{routing::{delete, get, post, put}, Router};
 
 pub fn product_routes() -> Router {
     Router::new().route("/api/product", post(create_product))
-                 .route("/api/get_all_products", get(get_all_products))
+                 .route("/api/get_products", get(get_all_products))
                  .route("/api/get_product/:uuid", get(get_product_by_uuid))
                  .route("/api/delete_product/:uuid", delete(delete_product))
                  .route("/api/product/:uuid", put(update_product))
